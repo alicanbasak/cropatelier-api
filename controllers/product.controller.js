9ab32470d9d320504efab49e25fd22e752435194
Alican Basak <alicanbasak@icloud.com>
2023-10-17 01:12:32 +0300

    Product controller fixed

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -11,6 +11,7 @@ export const createProduct = asyncHandler(async (req, res) => {
     req.body;
   // Product exists
   const productExists = await Product.findOne({ name });
+  // Check if product exists
   if (productExists) {
     throw new Error("Product already exists");
   }
@@ -41,19 +42,94 @@ export const createProduct = asyncHandler(async (req, res) => {
 // @access Public
 
 export const getAllProducts = asyncHandler(async (req, res) => {
+  // Apply filters and pagination
   const { query, pagination, total } = await applyFiltersAndPagination(
     Product.find(),
     req
   );
 
+  // Execute query
   const products = await query;
 
+  // Send response
   return res.json({
-    status: "success",
+    status: 200,
     message: "Products fetched successfully",
     total,
     pagination,
     result: products.length,
     products,
+  });
+});
+
+// @desc Get Single Product
+// @route GET /api/v1/products/:id
+// @access Private
+
+export const getProduct = asyncHandler(async (req, res) => {
+  // Find product
+  const product = await Product.findById(req.params.id);
+
+  // Check if product exists
+  if (!product) {
+    throw new Error("Product not found");
+  }
+
+  // Send response
+  return res.json({
+    status: 200,
+    message: "Product fetched successfully",
+    product,
+  });
+});
+
+// @desc Update Product
+// @route PUT /api/v1/products/:id
+// @access Private/Admin
+
+export const updateProduct = asyncHandler(async (req, res) => {
+  // Find product
+  const { name, description, category, sizes, colors, price, totalQty } =
+    req.body;
+
+  // Check if product exists
+  const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+      name,
+      description,
+      category,
+      sizes,
+      colors,
+      price,
+      totalQty,
+    },
+    {
+      // Return updated product
+      new: true,
+    }
+  );
+
+  // Send response
+  return res.json({
+    status: 200,
+    message: "Product updated successfully",
+    product,
+  });
+});
+
+// @desc Delete Product
+// @route DELETE /api/v1/products/:id
+// @access Private/Admin
+
+export const deleteProduct = asyncHandler(async (req, res) => {
+  // Find product
+  const product = await Product.findByIdAndDelete(req.params.id);
+
+  // Check if product exists
+  return res.json({
+    status: 200,
+    message: "Product deleted successfully",
+    product,
   });
 });
